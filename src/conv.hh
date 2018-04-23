39e38bbc11eeaebc4385a2132e341437d0fdce73
yottacto <yotta.w.cto@gmail.com>
2018-04-23 23:54:03 +0800

    [@] make filter size a templaet parameter

--- src/conv.hh
+++ src/conv.hh
@@ -32,18 +32,24 @@ struct multi_tmp<0, T>
     multi_tmp(value_type const&) {}
 };
 
-template <int I, int MAXI, int J, int MAXJ>
+// Size is filter size
+template <class T, int I, int MAXI, int J, int MAXJ, int Size>
 struct mult_block
 {
-    using next = mult_block<I, MAXI, J + 1, MAXJ>;
+    using next = mult_block<T, I, MAXI, J + 1, MAXJ, Size>;
 
     template <class Tmp, class MatA, class MatB>
-    void operator()(Tmp& tmp, MatA const& A, MatB const& B, int x, int y, int cols, int size)
+    void operator()(Tmp& tmp,
+        MatA const& A,
+        std::array<std::array<T, Size>, Size> const& B,
+        int x, int y, int cols)
     {
-        for (auto i = 0; i < size; i++)
-            for (auto j = 0; j < size; j++)
+        #pragma unroll
+        for (auto i = 0; i < Size; i++)
+            #pragma unroll
+            for (auto j = 0; j < Size; j++)
                 tmp.value += A[(x + I + i) * cols + y + J + j] * B[i][j];
-        next()(tmp.sub, A, B, x, y, cols, size);
+        next()(tmp.sub, A, B, x, y, cols);
     }
 
 
@@ -56,18 +62,23 @@ struct mult_block
 
 };
 
-template <int I, int MAXI, int MAXJ>
-struct mult_block<I, MAXI, MAXJ, MAXJ>
+template <class T, int I, int MAXI, int MAXJ, int Size>
+struct mult_block<T, I, MAXI, MAXJ, MAXJ, Size>
 {
-    using next = mult_block<I + 1, MAXI, 0, MAXJ>;
+    using next = mult_block<T, I + 1, MAXI, 0, MAXJ, Size>;
 
     template <class Tmp, class MatA, class MatB>
-    void operator()(Tmp& tmp, MatA const& A, MatB const& B, int x, int y, int cols, int size)
+    void operator()(Tmp& tmp,
+        MatA const& A,
+        std::array<std::array<T, Size>, Size> const& B,
+        int x, int y, int cols)
     {
-        for (auto i = 0; i < size; i++)
-            for (auto j = 0; j < size; j++)
+        #pragma unroll
+        for (auto i = 0; i < Size; i++)
+            #pragma unroll
+            for (auto j = 0; j < Size; j++)
                 tmp.value += A[(x + I + i) * cols + y + MAXJ + j] * B[i][j];
-        next()(tmp.sub, A, B, x, y, cols, size);
+        next()(tmp.sub, A, B, x, y, cols);
     }
 
     template <class Tmp, class Mat>
@@ -79,14 +90,19 @@ struct mult_block<I, MAXI, MAXJ, MAXJ>
 
 };
 
-template <int MAXI, int MAXJ>
-struct mult_block<MAXI, MAXI, MAXJ, MAXJ>
+template <class T, int MAXI, int MAXJ, int Size>
+struct mult_block<T, MAXI, MAXI, MAXJ, MAXJ, Size>
 {
     template <class Tmp, class MatA, class MatB>
-    void operator()(Tmp& tmp, MatA const& A, MatB const& B, int x, int y, int cols, int size)
+    void operator()(Tmp& tmp,
+        MatA const& A,
+        std::array<std::array<T, Size>, Size> const& B,
+        int x, int y, int cols)
     {
-        for (auto i = 0; i < size; i++)
-            for (auto j = 0; j < size; j++)
+        #pragma unroll
+        for (auto i = 0; i < Size; i++)
+            #pragma unroll
+            for (auto j = 0; j < Size; j++)
                 tmp.value += A[(x + MAXI + i) * cols + y + MAXJ + j] * B[i][j];
     }
 
@@ -121,7 +137,7 @@ void convolve(int rows, int cols, Vec const& din, Vec& dout,
 
     size = std::min(size, omp_get_max_threads());
 
-    impl::mult_block<0, BI - 1, 0, BJ - 1> block;
+    impl::mult_block<value_type, 0, BI - 1, 0, BJ - 1, filter_size> block;
 
     #pragma omp parallel for num_threads(size)
     for (auto x = 0; x < out_rows; x += BI)
